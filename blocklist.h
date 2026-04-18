2cc7f81c6989c2e263d3d3df8496069631e1c094
0wen11 <owensidarta@gmail.com>
2026-04-18 18:48:23 +0800

    dimension function

--- blocklist.h
+++ blocklist.h
@@ -2,6 +2,7 @@
 #define BLOCKLIST_H
 
 #include <string>
+#include <utility>
 #include <vector>
 
 const std::string blocklist[] = {
@@ -27,5 +28,6 @@ const std::string blocklist[] = {
 };
 
 std::string callblock(int id);
+std::pair<int, int> dimension(int id);
 
 #endif
