11cd4272f293c5ac3810f56c3e0e23c5bd2bc675
0wen11 <owensidarta@gmail.com>
2026-04-20 00:12:21 +0800

    refresh function

--- blocks.h
+++ blocks.h
@@ -6,7 +6,8 @@
 bool checkinsert();
 void hoverblock(int id);
 void insertblock();
-Gamedata playconfirm(int difficulty);
+Gamedata refresh(int difficulty);
+Gamedata playconfirm();
 Gamedata playchoose(char s);
 Gamedata playwasd(char s);
 void blockInit(int difficulty);
